7a7edf801dcb160138d9b54f04efeeadb9dfa79c
EunbinShin <38269665+EunbinShin@users.noreply.github.com>
2022-11-12 21:54:34 +0900

    Header 수정 ( 코드 간결 , css )

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,36 +1,34 @@
 function Header(props){
-    console.log('Header')
-
+    /* Header origin HTML
+    <header>
+        <div class="header header_left">
+            <span class="menu_name" id="menu_home">HOME</span>
+        </div>
+        <div class="header header_right">
+            <div class="menu_name" id="menu_signup">SIGNUP</div>
+        </div>
+    </header>
+     */
     this.header = document.createElement('header');
     
     this.home = document.createElement('div');
     this.home.className = 'header header_left'
-
-    this.home_span = document.createElement('span');
-    this.home_span.className = 'menu_name'
-    this.home_span.id = 'menu_home'
-    this.home_span.innerText = 'HOME'
+    this.home.innerHTML = `<span class="menu_name" id="menu_home">HOME</span>`
 
     this.signup = document.createElement('div');
     this.signup.className = 'header header_right'
-
-    this.signup_span = document.createElement('span');
-    this.signup_span.className = 'menu_name'
-    this.signup_span.id = 'menu_signup'
-    this.signup_span.innerText = 'SIGNUP'
-
-    this.home.appendChild(this.home_span)
-    this.signup.appendChild(this.signup_span)
+    this.signup.innerHTML = `<div class="menu_name" id="menu_signup">SIGNUP</div>`
 
     this.header.appendChild(this.home)
     this.header.appendChild(this.signup)
 
-    props.target.appendChild(this.header)
-
-    this.signup_span.addEventListener('click', ()=>{
+    props.$target.appendChild(this.header)
+    
+    this.signup.firstChild.addEventListener('click', ()=>{
         props.onClickSignup()
     })
-    this.home_span.addEventListener('click', ()=>{
+    
+    this.home.firstChild.addEventListener('click', ()=>{
         props.onClickHome()
     })
 }
