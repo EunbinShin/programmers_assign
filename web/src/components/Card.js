169e905661ce9447636b2261569f94a3c56d0615
EunbinShin <38269665+EunbinShin@users.noreply.github.com>
2022-11-12 22:52:51 +0900

    HomePage 구현 (기능 2.2.5 제외 )

--- web/src/components/Card.js
+++ web/src/components/Card.js
@@ -1,7 +1,7 @@
 function Card(props){
-    const cardList = document.createElement('div')
-    cardList.id = 'cards_container'
-    props.target.appendChild(cardList)
+    const $cardList = document.createElement('div')
+    $cardList.id = 'cards_container'
+    props.$target.appendChild($cardList)
 
     this.state = props.initialState
 
@@ -13,10 +13,10 @@ function Card(props){
     this.render = () => {
         if(!this.state)
             return
-        
-        cardList.innerHTML = `
+
+        $cardList.innerHTML = `
             ${this.state.map((data, idx) => 
-                `<div idx="${idx+1}" class="card">
+                `<div data-idx="${idx+1}" class="card ${data.status ? "is-flipped": ""}">
                     <div class="card_plane card_plane--front">${data.name}</div>
                     <div class="card_plane card_plane--back">${data.mbti}</div>
                  </div>`).join("")}
@@ -24,6 +24,23 @@ function Card(props){
     }
 
     this.render();
+    
+    $cardList.childNodes.forEach((card)=>{
+        card.addEventListener('click', (e)=>{
+            const idx = card.dataset.idx-1
+
+            this.state[idx].status = !this.state[idx].status
+
+            if(card.className.includes("is-flipped")){                
+                card.className = 'card'
+            }else{
+                const cardClass = card.className + ' is-flipped'
+                card.className = cardClass
+            }
+            localStorage.setItem('personalInfo', JSON.stringify(this.state))
+        })
+    })
+
 }
 
 export default Card
--- web/src/page/HomePage.js
+++ web/src/page/HomePage.js
@@ -3,36 +3,10 @@ import ContentTitle from "../components/ContentTitle.js";
 import { request } from "../util/api.js";
 
 function HomePage(props){
-    /*
-    <main id="page_content">
-        <div class="content_title">
-            <h1> CardView </h1>
-        </div>
-    </main>
-    */
-    this.state = props.initialState 
-    this.element = document.createElement('main')
-    this.element.id = 'page_content'
-
-    this.setState = (nextState) => {
-        this.state = nextState;
-        cards.setState(this.state)
-    }
-    
-    new ContentTitle({
-        target: this.element,
-        title: props.title
-    })  
-
-    const cards = new Card({
-        target: this.element,
-        initialState: this.state
-    })
-      
+    this.state = props.initialState
 
     const fetchData = async () => {
         const result = await request('src/data/new_data.json')
-        this.setState(result)
         const localResult = result.map((data, idx)=>{
             return{
                 idx: idx+1,
@@ -41,22 +15,39 @@ function HomePage(props){
                 nickname: data.nickname,
                 role: data.role,
                 mbti: data.mbti,
+                status: false
             }
         })
+        
+        this.setState(localResult)
         localStorage.setItem('personalInfo', JSON.stringify(localResult))
     }
 
-    this.render = () => {
-        const oldPage = document.querySelector('#page_content')
-        if(oldPage){
-            props.target.replaceChild(this.element, oldPage)
-        }else{
-            props.target.appendChild(this.element)
-        }
+    if(!this.state){
+        fetchData()
     }
 
-    this.state ? this.setState(props.initialState) : fetchData()
+    this.$element = document.createElement('main')
+    this.$element.id = 'page_content'
 
+    this.setState = (nextState) => {
+        this.state = nextState;
+        this.render()
+    }
+    
+    new ContentTitle({
+        $target: this.$element,
+        title: props.title,
+
+    })  
+
+    this.render = () => {
+        props.$target.appendChild(this.$element)
+        new Card({
+            $target: this.$element,
+            initialState: this.state
+        })
+    }
 }
 
 export default HomePage
